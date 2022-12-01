dd643262b8beb9277cacab2c85c4615b5773665f
Anatolii <anatolii.polovnikov@gmail.com>
2022-12-01 23:09:20 +0500

    Решил задачу 2

--- Program.cs
+++ Program.cs
@@ -96,3 +96,69 @@
 //7 2 2 6
 //2 3 4 7
 
+int[,] CreateRandom2DArray(int rows, int columns, int minValue, int maxValue)  
+{                                                                           
+  int[,] newArray = new int[rows, columns];  
+
+  for (int i = 0; i < rows; i++)
+  {
+     for(int j = 0; j < columns; j++)
+     {
+       newArray[i,j] = new Random().Next(minValue,maxValue + 1);
+     }
+  }
+  return newArray;    
+}
+
+void Show2DArray(int[,] array)
+{
+    for(int i = 0;i < array.GetLength(0) ;i++ ) 
+    {
+        for(int j = 0; j < array.GetLength(1); j++)  
+        {
+           Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine(); 
+    }
+}
+
+int [,] ReverseValueInArrayAroundDiadonale(int [,] array)
+{
+    // отсекаем оишбку ввода пользователя
+    if (array.GetLength(0) != array.GetLength(1))
+    {
+        Console.WriteLine("Невозможно развернуть массив. Массив должен быть квадратным (rows=colums)");
+        return array;
+    }
+
+    // меняем местами элементы вокруг главной диагонали
+    else
+    {
+        for (int i = 0; i < array.GetLength(0) - 1; i++) // идём по строчкам, исключаем элемент главной диагонали
+        {
+            for (int j = i + 1; j < array.GetLength(0); j++) // идём по столбцам
+            {
+                int temp = array[i,j];
+                array[i,j] = array[j,i];
+                array[j,i] = temp;
+            }
+        }
+    }
+    // возвращаем новый массив
+    return array;
+}
+
+// Пользовательский ввод
+Console.Write("Введите количество строк ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов ");
+int colums = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите минимальное значение ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное значение ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] oldArray = CreateRandom2DArray(rows, colums, min, max);
+Show2DArray (oldArray);
+oldArray = ReverseValueInArrayAroundDiadonale(oldArray);
+Show2DArray (oldArray);
